b808f8a809edf92d8a236474d3a8177cd8934bea
shohan-pherones <shohan.sub.56@gmail.com>
2024-12-04 23:16:33 +0600

    add staticfiles

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-]
-
-urlpatterns += staticfiles_urlpatterns()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
